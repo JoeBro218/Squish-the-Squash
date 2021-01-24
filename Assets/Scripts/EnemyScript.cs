dd15949956b1d6cd969f2c2c7cbdedfe0a46357e
unknown <you@example.com>
2021-01-24 13:44:36 -0500

    First commit

---
+++ Assets/Scripts/EnemyScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyScript : MonoBehaviour
+{
+    public Transform startMarker;
+    public Transform endMarker;
+    public float speed = 1.0F;
+    private float startTime;
+    private float journeyLength;
+
+    void Start()
+    {
+        startTime = Time.time;
+        journeyLength = Vector3.Distance(startMarker.position, endMarker.position);
+    }
+
+    void Update()
+    {
+
+        float distCovered = (Time.time - startTime) * speed;
+        float fracJourney = distCovered / journeyLength;
+        transform.position = Vector3.Lerp(startMarker.position, endMarker.position, Mathf.PingPong (fracJourney, 1));
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        PlayerController controller = other.GetComponent<PlayerController>();
+        if(controller != null)
+        {
+            controller.ChangeScore(1);
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,110 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class PlayerController : MonoBehaviour
+{
+    public Text score;
+    int scoreValue = 0;
+
+    public Text timer;
+    public int seconds = 10;
+    int introSeconds;
+    bool countDown = false;
+
+    public Text general;
+   
+    bool gameOver = true;
+
+    public AudioSource musicSource;
+    public AudioClip background;
+    public AudioClip winClip;
+    public AudioClip loseClip;
+
+    void Start()
+    {
+        introSeconds = seconds;
+        seconds = seconds + 2;
+        score.text = "Score: " + scoreValue;
+        timer.text = "" + seconds;
+
+        musicSource.clip = background;
+        musicSource.Play();
+        musicSource.loop = true;
+    }
+
+    void Update()
+    {
+        if (seconds == introSeconds)
+        {
+            gameOver = false;
+        }
+
+        if(gameOver == false)
+        {
+            float horizontal = Input.GetAxis("Horizontal");
+            Vector2 position = transform.position;
+            position.x = position.x + 0.1f * horizontal;
+            transform.position = position;
+        }
+
+        if(countDown == false && seconds>0)
+        {
+            StartCoroutine(Count());
+        }
+
+        if(gameOver == false && seconds == 0)
+        {
+            if(scoreValue == 4)
+            {
+                musicSource.loop = false;
+                musicSource.Stop();
+                musicSource.clip = winClip;
+                musicSource.Play();
+
+                general.text = "YOU WIN! Game by Joseph Demeritt.";
+                gameOver = true;
+            }
+            if(scoreValue != 4)
+            {
+                musicSource.loop = false;
+                musicSource.Stop();
+                musicSource.clip = loseClip;
+                musicSource.Play();
+
+                general.text = "You lose, press R to restart.";
+                gameOver = true;
+            }
+        }
+
+        if (Input.GetKey("escape"))
+        {
+            Application.Quit();
+        }
+
+        if (Input.GetKey(KeyCode.R))
+        {
+            if (gameOver == true)
+            {
+              SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex); 
+            }
+        }
+    }
+
+    public void ChangeScore(int amount)
+    {
+        scoreValue = scoreValue + amount;
+        score.text = "Score: " + scoreValue;
+    }
+
+    IEnumerator Count()
+    {
+        countDown = true;
+        yield return new WaitForSeconds(1);
+        seconds -= 1;
+        timer.text = "" + seconds;
+        countDown = false;
+    }
+}
